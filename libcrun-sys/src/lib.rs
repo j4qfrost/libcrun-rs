b7039d4042a28b6e96ac22d85ae00b64de437f5f
frosty <j4qfrost@gmail.com>
2021-03-05 05:10:52 -0800

    yes-std

--- libcrun-sys/src/lib.rs
+++ libcrun-sys/src/lib.rs
@@ -3,6 +3,5 @@
 #![allow(non_snake_case)]
 // suppress warnings about u128 (that we don't even use ourselves anyway)
 #![allow(improper_ctypes)]
-#![no_std]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
